829b21c009e247368b61b8454bf21f375789c9c3
Daorza <daokun04@gmail.com>
2025-06-07 10:44:31 +0700

    ?

--- lux_veritas/src/components/layout/Navbar.jsx
+++ lux_veritas/src/components/layout/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-scroll";
 
 export default function Navbar() {
     return (
-    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '1rem', backgroundColor: '#f8f9fa', width: '100%' }}>
-        <MainLink to="/">Lux Veritas</MainLink>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '1rem', backgroundColor: '#f8f9fa', width: '100%', verticalAlign: 'middle',  }}>
+        <MainLink to="/" style={{ padding: '1rem'}}>Lux Veritas</MainLink>
         <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
             <li>
                 <Link to="museum" smooth={true} duration={500}>Museum</Link>
